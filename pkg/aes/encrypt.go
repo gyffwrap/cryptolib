0bc846e98c6c51ef8a78b6fd25efb8492def3f2f
Muhammad Abdurrahman <rachman.sd@gmail.com>
2025-09-02 17:01:44 +0700

    feat: implement Encrypt function for AES encryption

---
+++ pkg/aes/encrypt.go
@@ -0,0 +1,64 @@
+package aes
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+
+	cryptErr "github.com/gyffwrap/cryptolib/pkg/aes/errors"
+)
+
+type Args struct {
+	Key      *string
+	ByteCode *int
+}
+
+func Encrypt(plaintext []byte, args *Args) (*AESResult, error) {
+	var keyBytes []byte
+	var byteCode int
+
+	if args != nil && args.Key != nil {
+		keyBytes = []byte(*args.Key)
+		byteCode = len(keyBytes)
+	} else {
+		if args != nil && args.ByteCode != nil {
+			byteCode = *args.ByteCode
+		} else {
+			byteCode = 32 // default AES-256
+		}
+
+		keyBytes = make([]byte, byteCode)
+		if _, err := io.ReadFull(rand.Reader, keyBytes); err != nil {
+			return nil, fmt.Errorf("%w: %v", cryptErr.ErrKeyGeneration, err)
+		}
+	}
+
+	if byteCode != 16 && byteCode != 32 {
+		return nil, cryptErr.ErrInvalidKeySize
+	}
+
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %v", cryptErr.ErrCipherInit, err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %v", cryptErr.ErrGCMInit, err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, fmt.Errorf("%w: %v", cryptErr.ErrNonceGeneration, err)
+	}
+
+	ciphertext := gcm.Seal(nil, nonce, plaintext, nil)
+
+	return &AESResult{
+		Ciphertext: ciphertext,
+		Key:        keyBytes,
+		Nonce:      nonce,
+	}, nil
+}

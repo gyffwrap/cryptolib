9e42ac1d69ff80ee93d69f644b847d7879236ef5
Muhammad Abdurrahman <rachman.sd@gmail.com>
2025-09-03 18:29:43 +0700

    feat(aes): add base64 and raw getters for AESResult
    Provide direct access to ciphertext, key, and nonce, along with their base64 encoded representations for easier handling and display.

--- pkg/aes/map.go
+++ pkg/aes/map.go
@@ -1,7 +1,33 @@
 package aes
 
+import "encoding/base64"
+
 type AESResult struct {
 	Ciphertext []byte
 	Key        []byte
 	Nonce      []byte
+}
+
+func (r *AESResult) GetCiphertext() []byte {
+	return r.Ciphertext
+}
+
+func (r *AESResult) GetKey() []byte {
+	return r.Key
+}
+
+func (r *AESResult) GetNonce() []byte {
+	return r.Nonce
+}
+
+func (r *AESResult) GetCiphertextBase64() string {
+	return base64.StdEncoding.EncodeToString(r.Ciphertext)
+}
+
+func (r *AESResult) GetKeyBase64() string {
+	return base64.StdEncoding.EncodeToString(r.Key)
+}
+
+func (r *AESResult) GetNonceBase64() string {
+	return base64.StdEncoding.EncodeToString(r.Nonce)
 }
